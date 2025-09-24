0a41da677b0da5e5a6db54f21cc7da1f9bea8e54
websitefood <hiranmoyxyz@gmail.com>
2025-09-24 22:34:41 +0530

    Update and rename App.js to main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import './i18n'; // Initialize i18n
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
